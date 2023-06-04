8b297b91aa728c25845fc29ebcd5a407c950a0bd
Neelachari N H <neelachari34@gmail.com>
2023-06-04 12:34:21 +0530

    Dark-Mode

--- script.js
+++ script.js
@@ -82,3 +82,30 @@ const typed = new Typed('.multiple-text', {
   backdelay: 1000,
   loop: true
 })
+
+
+const btn = document.querySelector('.toggleBtn')
+const moveBtn = document.querySelector('.bgMove')
+const fa = document.querySelector('.fa-solid')
+const faMoon = document.querySelector('.fa-moon')
+
+btn.addEventListener('click', function () {
+    moveBtn.classList.toggle('active')
+
+    fa.classList.toggle('dark')
+
+    faMoon.classList.toggle('active')
+    if (moveBtn.classList == 'active') {
+        faMoon.classList.add('moon-active')
+    }
+    else {
+        faMoon.classList.remove('moon-active')
+    }
+    // Apply condition when toggle button is active and deactive
+    if (moveBtn.classList.contains('active')) {
+        document.body.classList.add('dark')
+    }
+    else {
+        document.body.classList.remove('dark')
+    }
+})
